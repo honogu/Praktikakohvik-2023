4dea9a032ea818af9c6084921ed5453e28f64606
Holger Nõgu <honogu@ttu.ee>
2023-01-20 15:25:40 +0200

    changes

--- src/components/FirmsInfo.js
+++ src/components/FirmsInfo.js
@@ -1,21 +1,9 @@
 import './FirmsInfo.css';
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import { srcContext } from "../SrcContext.js";
 
 const FirmsInfo = ({ selectedFirm, isLoading, setLoading }) => {
     const { value, language } = useContext(srcContext);
-    const [ loadingText, setLoadingText ] = useState('');
-
-    useEffect(() => {
-		const interval = setInterval(function() {
-			if (isLoading)
-			{
-				setLoadingText(setLoadingText => setLoadingText === ' . . .' ? '' : setLoadingText += ' .')
-			}
-			else setLoadingText(setLoadingText => setLoadingText = '')
-		}, 450);
-		return () => clearInterval(interval)
-    }, [isLoading]);
 
     return (
 		<>
@@ -30,9 +18,7 @@ const FirmsInfo = ({ selectedFirm, isLoading, setLoading }) => {
 			</div>
 			<p>
 				{
-					isLoading
-					?	language.firmList.Loading + loadingText
-					:	(value === 'et' ? selectedFirm.estonianDescription : selectedFirm.englishDescription) ?? language.firmList.Guide 
+					(value === 'et' ? selectedFirm.estonianDescription : selectedFirm.englishDescription) ?? language.firmList.Guide 
 				}
 			</p>
 		</>
