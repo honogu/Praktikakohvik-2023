6fbe1ec70ea307d944847123c027b4718533b430
Holger Nõgu <honogu@ttu.ee>
2023-01-22 21:31:16 +0200

    changes

--- src/components/FirmsInfo.js
+++ src/components/FirmsInfo.js
@@ -1,14 +1,10 @@
 import './FirmsInfo.css';
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { srcContext } from "../SrcContext.js";
 
 const FirmsInfo = ({ selectedFirm }) => {
     const { value, language } = useContext(srcContext);
     const [ imageLoading, setImageLoading] = useState(true);
-
-	useEffect(() => {
-		setImageLoading(true)
-    }, [selectedFirm]);
 
     return (
 		<>
@@ -19,7 +15,7 @@ const FirmsInfo = ({ selectedFirm }) => {
 				<img
 					src={'https://pkapi.onrender.com/api/firms/' + selectedFirm.id + '/image/1'}
 					style={ imageLoading ? { display: 'none' } : { display: 'block' } }
-					onLoad={() => { setImageLoading(false); saveLoadedImage(selectedFirm.id) } }
+					onLoad={() => setImageLoading(false) }
 					alt="firmsLogo"
 				/>
 			</div>
@@ -36,10 +32,3 @@ const FirmsInfo = ({ selectedFirm }) => {
 };
 
 export default FirmsInfo;
-
-let images = []
-function saveLoadedImage(id) {
-	let img = new Image()
-	img.src = 'https://pkapi.onrender.com/api/firms/' + id + '/image/1';
-	images.push(img)
-}
